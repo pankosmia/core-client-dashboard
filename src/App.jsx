62cbdf5a1c0fbd5b9875c346d1ec85c5c34fd554
Mark Howe <mvahowe@gmail.com>
2025-04-29 21:57:30 +0200

    Bump pithekos

--- src/App.jsx
+++ src/App.jsx
@@ -19,53 +19,58 @@ function App() {
     const {enabledRef} = useContext(netContext);
     const {debugRef} = useContext(debugContext);
 
-    return <Grid2 
-                container
-                spacing={2}
-                sx={{
-                alignItems: "flex-start"
-                }}
-           >
-                <Grid2 item size={12}>
-                    <p><b>{doI18n("pages:core-dashboard:summary", i18nRef.current)}</b></p>
-                </Grid2>
-                    {
-                        clients
-                            .filter(c => !c.id.includes("dashboard"))
-                            .filter(c => !c.exclude_from_dashboard)
-                            .filter(c => (c.requires.debug && debugRef.current) || !c.requires.debug)
-                            .map(
-                                c => <Grid2 
-                                        item
-                                        size={{ xs: 12, sm: 6, lg: 4 }}
-                                    >
-                                        <Card>
-                                            <CardActionArea
-                                                onClick={()=> {
-                                                    if (enabledRef.current || !c.requires.net) {
-                                                        window.location.href = c.url
-                                                    }
-                                                }}
-                                                sx={{ 
-                                                    border: (enabledRef.current || !c.requires.net) ? "1px #000 solid" : "1px #9E9E9E solid",
-                                                    borderRadius: "5px",
-                                                    backgroundColor: "#FFF",
-                                                    color: (enabledRef.current || !c.requires.net) ? "#000" : "#9E9E9E",
+    return <Grid2
+        container
+        spacing={2}
+        justifyItems = "flex-end"
+        alignItems = "stretch"
+    >
+        <Grid2 item size={12}>
+            <p><b>{doI18n("pages:core-dashboard:summary", i18nRef.current)}</b></p>
+        </Grid2>
+        {
+            clients
+                .filter(c => !c.id.includes("dashboard"))
+                .filter(c => !c.exclude_from_dashboard)
+                .filter(c => (c.requires.debug && debugRef.current) || !c.requires.debug)
+                .map(
+                    c => <Grid2
+                        justifyItems = "flex"
+                        justifyDirection = "row"
+                        alignItems = "stretch"
+                        container
+                        item
+                        size={{xs: 12, sm: 6, lg: 4}}
+                    >
+                        <Card sx={{height: "auto"}}>
+                            <CardActionArea
+                                onClick={() => {
+                                    if (enabledRef.current || !c.requires.net) {
+                                        window.location.href = c.url
+                                    }
+                                }}
+                                sx={{
+                                    height: "auto",
+                                    border: (enabledRef.current || !c.requires.net) ? "1px #000 solid" : "1px #9E9E9E solid",
+                                    borderRadius: "5px",
+                                    backgroundColor: "#FFF",
+                                    color: (enabledRef.current || !c.requires.net) ? "#000" : "#9E9E9E",
 
-                                                    '&:hover': { backgroundColor: '#F5F5F5' } }}
-                                                disabled={(enabledRef.current || !c.requires.net) ? false : true }
-                                            >
-                                                <CardContent>
-                                                    <h2>
-                                                        {doI18n(`pages:${c.id}:title`, i18nRef.current)}
-                                                    </h2>
-                                                    <p>{doI18n(`pages:${c.id}:summary`, i18nRef.current)}</p>
-                                                </CardContent>
-                                            </CardActionArea>
-                                        </Card>
-                                    </Grid2>
-                            )
-                        }
+                                    '&:hover': {backgroundColor: '#F5F5F5'}
+                                }}
+                                disabled={(enabledRef.current || !c.requires.net) ? false : true}
+                            >
+                                <CardContent sx={{height: "auto"}}>
+                                    <h2>
+                                        {doI18n(`pages:${c.id}:title`, i18nRef.current)}
+                                    </h2>
+                                    <p>{doI18n(`pages:${c.id}:summary`, i18nRef.current)}</p>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid2>
+                )
+        }
     </Grid2>
 }
 
