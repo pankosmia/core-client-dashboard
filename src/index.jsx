a9392bf8fffda0aeecf77eef02faf1cab870f470
Mark Howe <mvahowe@gmail.com>
2026-02-19 11:05:13 +0100

    Format with JetBrains

--- src/index.jsx
+++ src/index.jsx
@@ -1,35 +1,29 @@
-import { createRoot } from "react-dom/client";
-import { SpSpa,SpaContainer } from "pankosmia-rcl";
+import {createRoot} from "react-dom/client";
+import {SpSpa, SpaContainer} from "pankosmia-rcl";
 import App from "./App";
 import './index.css';
-import { createTheme, styled } from "@mui/material";
-import { ThemeProvider } from "@emotion/react";
-import { SnackbarProvider,  MaterialDesignContent, } from "notistack";
-import { getAndSetJson } from "pithekos-lib";
-import { useEffect, useState } from "react";
+import {createTheme, styled} from "@mui/material";
+import {ThemeProvider} from "@emotion/react";
+import {SnackbarProvider, MaterialDesignContent,} from "notistack";
+import {getAndSetJson} from "pithekos-lib";
+import {useEffect, useState} from "react";
+
 function AppLayout() {
 
     const [themeSpec, setThemeSpec] = useState({
         palette: {
             primary: {
                 main: "#666",
-            },
-            secondary: {
+            }, secondary: {
                 main: "#888",
             },
         },
     });
 
     useEffect(() => {
-        if (
-            themeSpec.palette &&
-            themeSpec.palette.primary &&
-            themeSpec.palette.primary.main &&
-            themeSpec.palette.primary.main === "#666"
-        ) {
+        if (themeSpec.palette && themeSpec.palette.primary && themeSpec.palette.primary.main && themeSpec.palette.primary.main === "#666") {
             getAndSetJson({
-                url: "/app-resources/themes/default.json",
-                setter: setThemeSpec,
+                url: "/app-resources/themes/default.json", setter: setThemeSpec,
             }).then();
         }
     }, []);
@@ -37,20 +31,13 @@ function AppLayout() {
     const theme = createTheme(themeSpec);
     const CustomSnackbarContent = styled(MaterialDesignContent)(() => ({
         "&.notistack-MuiContent-error": {
-            backgroundColor: "#FDEDED",
-            color: "#D32F2F",
-        },
-        "&.notistack-MuiContent-info": {
-            backgroundColor: "#E5F6FD",
-            color: "#0288D1",
-        },
-        "&.notistack-MuiContent-warning": {
-            backgroundColor: "#FFF4E5",
-            color: "#EF6C00",
-        },
-        "&.notistack-MuiContent-success": {
-            backgroundColor: "#EDF7ED",
-            color: "#2E7D32",
+            backgroundColor: "#FDEDED", color: "#D32F2F",
+        }, "&.notistack-MuiContent-info": {
+            backgroundColor: "#E5F6FD", color: "#0288D1",
+        }, "&.notistack-MuiContent-warning": {
+            backgroundColor: "#FFF4E5", color: "#EF6C00",
+        }, "&.notistack-MuiContent-success": {
+            backgroundColor: "#EDF7ED", color: "#2E7D32",
         },
     }));
     return <ThemeProvider theme={theme}>
@@ -64,18 +51,17 @@ function AppLayout() {
             maxSnack={6}
         >
             <SpaContainer>
-                <App />
+                <App/>
             </SpaContainer>
         </SnackbarProvider>
     </ThemeProvider>
 }
+
 createRoot(document.getElementById("root"))
-    .render(
-        <SpSpa
-            requireNet={false}
-            titleKey="pages:core-dashboard:title"
-            currentId="core-dashboard"
-        >
-            <AppLayout/>
-        </SpSpa>
-    );
+    .render(<SpSpa
+        requireNet={false}
+        titleKey="pages:core-dashboard:title"
+        currentId="core-dashboard"
+    >
+        <AppLayout/>
+    </SpSpa>);
