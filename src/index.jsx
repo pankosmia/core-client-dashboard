8969f7b5b8f459919ab6c9795179100c480b336d
Mark Howe <mvahowe@gmail.com>
2026-02-19 11:06:26 +0100

    JetBrains reformat

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,13 @@
-import { createRoot } from "react-dom/client";
-import { SpSpa } from "pankosmia-rcl";
+import {createRoot} from "react-dom/client";
+import {SpSpa} from "pankosmia-rcl";
 import App from "./App";
 import './index.css';
 
 createRoot(document.getElementById("root"))
-    .render(
-        <SpSpa
-            requireNet={false}
-            titleKey="pages:core-dashboard:title"
-            currentId="core-dashboard"
-        >
-            <App />
-        </SpSpa>
-    );
+    .render(<SpSpa
+        requireNet={false}
+        titleKey="pages:core-dashboard:title"
+        currentId="core-dashboard"
+    >
+        <App/>
+    </SpSpa>);
