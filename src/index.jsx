105ef5b04ec042b8671a7e48b05ddb659dbc9035
Gabriel Aillet <aillet.gabriel05@gmail.com>
2026-04-07 14:53:33 +0200

    prettier husky

--- src/index.jsx
+++ src/index.jsx
@@ -1,33 +1,39 @@
 import { createRoot } from "react-dom/client";
 import { SpSpa, fallbackTheme } from "pankosmia-rcl";
 import App from "./App";
-import './index.css';
+import "./index.css";
 import { useEffect, useState } from "react";
 import { getAndSetJson } from "pithekos-lib";
 import { ThemeProvider } from "@emotion/react";
 import { createTheme } from "@mui/material";
 function AppLayout() {
-    const [themeSpec, setThemeSpec] = useState(fallbackTheme);
-    const theme = createTheme(themeSpec);
-    useEffect(() => {
-        if (themeSpec.palette && themeSpec.palette.primary && themeSpec.palette.primary.main && themeSpec.palette.primary.main === "#666") {
-            getAndSetJson({
-                url: "/app-resources/themes/default.json", setter: setThemeSpec,
-            }).then();
-        }
-    }, []);
+  const [themeSpec, setThemeSpec] = useState(fallbackTheme);
+  const theme = createTheme(themeSpec);
+  useEffect(() => {
+    if (
+      themeSpec.palette &&
+      themeSpec.palette.primary &&
+      themeSpec.palette.primary.main &&
+      themeSpec.palette.primary.main === "#666"
+    ) {
+      getAndSetJson({
+        url: "/app-resources/themes/default.json",
+        setter: setThemeSpec,
+      }).then();
+    }
+  }, []);
 
-    return <ThemeProvider theme={theme}>
-        <SpSpa
-            requireNet={false}
-            titleKey="pages:core-dashboard:title"
-            currentId="core-dashboard">
-            <App />
-        </SpSpa>
+  return (
+    <ThemeProvider theme={theme}>
+      <SpSpa
+        requireNet={false}
+        titleKey="pages:core-dashboard:title"
+        currentId="core-dashboard"
+      >
+        <App />
+      </SpSpa>
     </ThemeProvider>
+  );
 }
 
-createRoot(document.getElementById("root"))
-    .render(
-        <AppLayout />
-    );
+createRoot(document.getElementById("root")).render(<AppLayout />);
